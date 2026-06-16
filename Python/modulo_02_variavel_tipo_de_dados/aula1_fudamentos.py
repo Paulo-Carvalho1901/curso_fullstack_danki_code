4d6803dd6e083c114068aa1f64bc058ab45a66b4
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-06-16 00:50:22 -0300

    (intro) sobre o que é cnceito de algoritmo

---
+++ Python/modulo_02_variavel_tipo_de_dados/aula1_fudamentos.py
@@ -0,0 +1,26 @@
+# O que é Algoritmo
+
+# Um algoritmo é um conjunto de instruções passo a passo, organizado de forma 
+# lógica, para resolver um problema ou realizar uma tarefa específica
+
+# Ex:
+
+# Fazer um Bolo
+
+# (Insentivar o time resolver o problma juntos)
+
+# 1 - pegar os ingredientes
+# 2 - pegar uma vazilia
+# 3 - misturar os ingredientes
+# 4 - pegar uma forma para a massa
+# 5 - ligar o forno
+# 6 - colocar a massa na forma
+# 7 - colocar a forma no forno
+# 8 - esperar 40 minutos
+# 9 - Fim
+
+# Conceito Famoso
+# Na programação, esse conceito de pegar um problema grande e quebrá-lo em pedaços
+# menores é conhecido como Divisão e Conquista (ou Divide and Conquer, em inglês).
+# Essa técnica é a base para escrever códigos limpos, fáceis de manter e eutilizáveis
+
