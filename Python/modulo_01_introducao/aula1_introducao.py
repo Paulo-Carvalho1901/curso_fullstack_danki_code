cd95512ae0c83831705fd3d0ee5770596e92b4f3
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-06-16 00:36:25 -0300

    (intro) curso de Pytthon

---
+++ Python/modulo_01_introducao/aula1_introducao.py
@@ -0,0 +1,78 @@
+# O que é Python?
+# Python é uma linguagem interpretada de auto nivel e que suporta
+# múltiplos paradigmas de programação: imperativo, orientado a 
+# Objetos e funcional. E uma linguagem com tipagem dinânica e forte
+
+# A linguagem foi projetada com a filosofia de enfatizar a importâancia
+# do esforço do programador sobre o esforço computacional
+# Prioriza a legibilidade do código sobre a velocidade ou expressividade
+# combinado com sintaxe consisa e clara com os recursos poderosos
+# de sua biblioteca padrão e por módulos e frameworks desenvolvidoe por
+# terceiros.
+
+# Aplicações da lingagem Python
+
+# 1 - Desenvolvimento Web
+# 2 - Ciências de dados
+# 3 - Hacking/segurança da informação
+# 4 - Automação
+# 5 IA
+# 6 Jogos
+
+# Empresas que utilizam o Pythn
+
+# 1 - Printerest - Catalogo digital de ideias
+# 2 - Instagram
+# 3 - Mozilla
+# 4 - Blender - software gratuito de modelagem 3D para desenvolvimento de jogos 
+
+# Entrar no Menu interativo do Python
+# python -c "import this"
+
+"""
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+
+TRADUÇÃO
+
+A Arte Zen do Python, por Tim Peters
+
+Bonito é melhor que feio.
+Explícito é melhor que implícito.
+Simples é melhor que complexo.
+Complexo é melhor que complicado.
+Plano é melhor que aninhado.
+Esparso é melhor que denso.
+Legibilidade importa.
+Casos especiais não são especiais o suficiente para quebrar as regras.
+Embora a praticidade seja melhor que a pureza.
+Erros nunca devem passar despercebidos.
+A menos que sejam explicitamente silenciados.
+Diante da ambiguidade, resista à tentação de adivinhar.
+Deve haver uma — e de preferência apenas uma — maneira óbvia de fazer algo.
+Embora essa maneira possa não ser óbvia à primeira vista, a menos que você seja holandês.
+Agora é melhor que nunca.
+Embora nunca seja muitas vezes melhor que *agora*.
+Se a implementação for difícil de explicar, é uma má ideia.
+Se a implementação for fácil de explicar, pode ser uma boa ideia.
+Namespaces são uma ideia genial — vamos usar mais deles!
+"""
