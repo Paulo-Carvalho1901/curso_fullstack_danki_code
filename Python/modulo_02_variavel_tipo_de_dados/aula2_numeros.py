970c16e7703f89406cfc7feca829912348b8ceff
Paulo Carvalho <paulo.carvalho@global.ntt>
2026-06-17 13:17:08 -0300

    aula: aprendendo sobre numeros em Python float e int

---
+++ Python/modulo_02_variavel_tipo_de_dados/aula2_numeros.py
@@ -0,0 +1,11 @@
+# Se tratando de números o Python basicamente
+# trabalha com dois tipos de números
+# int ou integer, que são os números inteiros
+# float ou números com ponto fluante. que são com casas decimais
+
+# ex:
+# int
+num_int = -3, -2, -1, 0, 1, 2, 3
+
+# float
+num_flot = -3.0, -2.5, 1.2, 0.0, 15.45, 21.33
